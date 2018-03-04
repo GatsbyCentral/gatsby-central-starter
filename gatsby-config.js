fcfa1ed1e96653d52d00c9fb2ac982a277f35374
sarasate <luis@superlumen.io>
2018-03-04 14:07:48 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby Central Starter"
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-resolve-src",
+    "gatsby-plugin-styled-components"
+  ]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "gatsby-link";
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>
+      This is a starter project for gatsby that we us for our on purpose. Hence
+      it is very opinionated.
+    </p>
+    <p>Now go build something great.</p>
+    <Link to="/about/">Gatsby Central</Link>
+  </div>
+);
+
+export default IndexPage;
