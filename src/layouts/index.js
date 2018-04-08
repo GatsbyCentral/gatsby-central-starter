decce546ebc954c89b0b27767d2f50acf6257b2d
sarasate <luis@superlumen.io>
2018-04-08 23:22:29 +0200

    Replace inline styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
+import styled from "styled-components";
 
 import Header from "../components/Header";
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`;
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -14,16 +22,7 @@ const TemplateWrapper = ({ children }) => (
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 960,
-        padding: "0px 1.0875rem 1.45rem",
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    <Content>{children()}</Content>
   </div>
 );
 
